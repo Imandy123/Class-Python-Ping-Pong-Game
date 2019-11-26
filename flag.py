92306b39c41bf2002b5dd664692de28c4bc3031d
Donal George <54420049+dgeorge7@users.noreply.github.com>
2019-11-25 20:28:48 -0500

    Update flag.py

--- flag.py
+++ flag.py
@@ -1,14 +1,8 @@
+import pygame
 class Flag(pygame.sprite.Sprite):
-      
-      def __init__(self, name, pos, image):
-          pygame.sprite.Sprite.__init__(self) 
-          self.name = name
-          self.image = pygame.image.load("flag.png")
-          self.rect = self.image.get_rect()
-          self.rect.x = pos[0]
-          self.rect.y = pos[1]
-
-       def caption(self, name):
-           myfont = pygame.font.SysFont(None, 50)
-           message = myfont.render(name, False, (0,0,0))
-           self.screen.blit(message, (0, 0))
+    def __init__(self, x, y):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load("assets/iflag.png")
+        self.rect = self.image.get_rect()
+        self.rect.x = y
+        self.rect.y = y
