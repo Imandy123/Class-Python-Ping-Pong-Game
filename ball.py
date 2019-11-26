0983e7db04c4680d052da0612842b2c46c483df0
Donal George <54420049+dgeorge7@users.noreply.github.com>
2019-11-25 20:13:23 -0500

    Update ball.py

--- ball.py
+++ ball.py
@@ -1,23 +1,25 @@
+import pygame
 class Ball(pygame.sprite.Sprite):
-  
-      def __init__(self, name, pos, image):
-          pygame.sprite.Sprite.__init__(self) 
-          self.name = name
-          self.image = pygame.image.load("ball.png")
-          self.rect = self.image.get_rect()
-          self.rect.x = pos[0]
-          self.rect.y = pos[1]
- 
-      def speedUp(self, collisions):
-          if collisions:
-             for i in collisions:
-                 self.rect.y += 5
+    def __init__(self, x, y):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load('assets/ball.png')
+        self.image = pygame.transform.scale(self.image, (20, 20))
+        self.mask = pygame.mask.from_surface(self.image)
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+        self.direction = 0
+        self.transdown = self.rect.centery + 300
+        self.transup = self.rect.centery -300
 
-      def bounce(self, table):
-          if table.rect.center == ball.rect.center:
-             self.rect.y -= 5
-             self.screen.blit(self.image)
-             self.rect.y += 5
-             self.screen.blit(self.image)
+    def move(self, py, trans, speed):
+           if(self.rect.centery < py and self.rect.centery > self.transup): 
+              self.rect.centery -= 1
+              return True      
+           if(self.rect.centery > py and self.rect.centery < self.transdown):
+              self.rect.centery += 1
+              return True
+           else:
+              return False
 
 
