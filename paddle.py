b009a2fbed55f108254f054dcb4b3e3b57c2fce1
Donal George <54420049+dgeorge7@users.noreply.github.com>
2019-12-03 02:39:11 -0500

    Add files via upload

--- paddle.py
+++ paddle.py
@@ -1,10 +1,10 @@
 import pygame
 class Paddle(pygame.sprite.Sprite):
-     def __init__(self, name, pos):
+     def __init__(self, name, image, pos):
         pygame.sprite.Sprite.__init__(self) 
         self.name = name
-        self.imageOr = pygame.image.load("assets/paddle.png")
-        self.imageOr = pygame.transform.scale(self.imageOr, (90, 100))
+        self.imageOr = pygame.image.load(image)
+        self.imageOr = pygame.transform.scale(self.imageOr, (130, 120))
         self.imageOr = pygame.transform.rotate(self.imageOr, -20)
         if name == "Opp":
             self.imageOr = pygame.transform.flip(self.imageOr, False, True)
@@ -24,10 +24,8 @@ class Paddle(pygame.sprite.Sprite):
             
      def sync(self, swidth):    
             if self.rect.centerx < swidth:
-                self.angle = 120/(swidth)
+                self.angle = 40/(swidth)
                 if self.name == "Opp":
                     self.rot(-(self.angle*((swidth)-self.rect.centerx)))
                 else:
                    self.rot(self.angle*((swidth)-self.rect.centerx))
-
-          
