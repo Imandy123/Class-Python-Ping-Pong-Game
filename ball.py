d457c7f386e63c54dfd06bc662789ae3687b43d6
Donal George <54420049+dgeorge7@users.noreply.github.com>
2019-12-05 18:06:27 -0500

    Add files via upload

--- ball.py
+++ ball.py
@@ -1,4 +1,5 @@
 import pygame
+import math
 class Ball(pygame.sprite.Sprite):
     def __init__(self, x, y):
         pygame.sprite.Sprite.__init__(self)
@@ -8,18 +9,32 @@ class Ball(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.rect.x = x
         self.rect.y = y
-        self.direction = 0
-        self.transdown = self.rect.centery + 300
-        self.transup = self.rect.centery -300
+        self.vel = pygame.math.Vector2(0, 0)
+      
+    def vec(self, py, speed):
+           if py[1] < self.rect.centery and py[0] > self.rect.centerx: #ball bottom and left
+                self.vel = pygame.math.Vector2(-speed*2, speed)
+           elif py[1] < self.rect.centery and py[0] < self.rect.centerx: # ball bottom and right
+                  self.vel = pygame.math.Vector2(speed*2, speed)
+           elif py[1] > self.rect.centery and py[0] < self.rect.centerx: # ball above and right
+                  self.vel = pygame.math.Vector2(speed*2, -speed)
+           elif py[1] > self.rect.centery and py[0] > self.rect.centerx: #ball above and left
+                  self.vel = pygame.math.Vector2(-speed*2, -speed)
+           elif py[1] == self.rect.centery and py[0] < self.rect.centerx: #ball horizontal and right
+                  self.vel = pygame.math.Vector2(speed, 0)
+           elif py[1] == self.rect.centery and py[0] < self.rect.centerx:  #ball horizontal and left
+                  self.vel = pygame.math.Vector2(-speed, 0)
+           elif py[1] < self.rect.centery and py[0] == self.rect.centerx: #ball vertical and bottom
+                  self.vel = pygame.math.Vector2(0, speed)
+           elif py[1] > self.rect.centery and py[0] == self.rect.centerx: #ball vertical and top
+                  self.vel = pygame.math.Vector2(0, -speed)
+           return self.vel
+               
+   
+    def bounce(self, vel):
+           #self.vel = pygame.math.Vector2(math.cos(angle)*speed, math.sin(angle)*speed)
+           self.rect.center +=  self.vel 
+           return True   
+           
 
-    def move(self, py, trans, speed):
-           if(self.rect.centery < py and self.rect.centery > self.transup): 
-              self.rect.centery -= 1
-              return True      
-           if(self.rect.centery > py and self.rect.centery < self.transdown):
-              self.rect.centery += 1
-              return True
-           else:
-              return False
-
-
+       
--- paddle.py
+++ paddle.py
@@ -1,17 +1,21 @@
 import pygame
+import random
 class Paddle(pygame.sprite.Sprite):
      def __init__(self, name, image, pos):
         pygame.sprite.Sprite.__init__(self) 
         self.name = name
         self.imageOr = pygame.image.load(image)
         self.imageOr = pygame.transform.scale(self.imageOr, (130, 120))
-        if name == "Opp":
+        if self.name == "Opp":
             self.imageOr = pygame.transform.flip(self.imageOr, False, True)
         self.image = self.imageOr.copy()
         self.mask = pygame.mask.from_surface(self.image)
         self.rect = self.image.get_rect()
         self.rect.x = pos[0]
         self.rect.y = pos[1]
+        self.points = 0
+        self.randy= random.randrange(50, 300)
+
      
      def rot(self, angle):
             prevCent = self.rect.center
@@ -19,6 +23,24 @@ class Paddle(pygame.sprite.Sprite):
             self.mask = pygame.mask.from_surface(self.image)
             self.rect = self.image.get_rect()
             self.rect.center = prevCent
+    
+     def ai(self,  speed):
+             if(self.randy < self.rect.centery): 
+               if(self.rect.centery - self.randy < speed): 
+                  self.rect.centery -= 1
+               else:
+                  self.rect.centery -= speed
+               return True                   
+             elif(self.randy > self.rect.centery):
+               if(self.randy - self.rect.centery < speed): 
+                  self.rect.centery += 1
+               else:
+                  self.rect.centery += speed
+               return True
+             else:
+                 self.randy= random.randrange(50, 300)
+                 return False
+
                 
             
      def sync(self, swidth):    
