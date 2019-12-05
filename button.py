45e1b1d264aace7113fd1f65d4efe183df300e91
Donal George <54420049+dgeorge7@users.noreply.github.com>
2019-12-05 18:05:08 -0500

    Add files via upload

--- button.py
+++ button.py
@@ -1,18 +1,9 @@
 import pygame
 class Button(pygame.sprite.Sprite):
-      
-      def __init__(self, name, pos, image, altimg):
+    def __init__(self,  pos, image, altimg):
           pygame.sprite.Sprite.__init__(self) 
-          self.name = name
-          self.images = [pygame.image.load("button.png"), pygame.image.load(altimg)]
+          self.images = [pygame.image.load(image), pygame.image.load(altimg)]
           self.image = self.images[0] 
           self.rect = self.image.get_rect()
           self.rect.x = pos[0]
           self.rect.y = pos[1]
-     
-      def clicked(self, mospos): #Checks if mouse clicks button, changes image slightly and returns boolean
-          if mospos == self.rect.center:
-             self.image = self.images[1]
-             self.rect = self.image.get_rect()
-             self.screen.blit(self.image)
-             return True
