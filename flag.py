386a5a23d7ddeb0a05e995985d1aecc817dda49c
Donal George <54420049+dgeorge7@users.noreply.github.com>
2019-12-06 11:06:38 -0500

    Delete flag.py

--- flag.py
+++
@@ -1,8 +0,0 @@
-import pygame
-class Flag(pygame.sprite.Sprite):
-    def __init__(self, x, y):
-        pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.image.load("assets/iflag.png")
-        self.rect = self.image.get_rect()
-        self.rect.x = y
-        self.rect.y = y
